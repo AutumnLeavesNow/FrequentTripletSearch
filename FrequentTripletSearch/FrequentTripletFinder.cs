2c69afd635d23eba223eeb6fa51d3fcdee4ca8d1
Admin <Admin@DELMARCO>
2017-08-14 13:14:48 +0300

    Add project files.

---
+++ FrequentTripletSearch/FrequentTripletFinder.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrequentTripletSearch
+{
+
+    class FrequentTripletFinder
+    {
+        private static object syncRoot = new object();
+        public string MostFrequentTriplet(string str, CancellationToken ct)
+        {
+            string[] strings = str.Split(',');
+            Dictionary<string, int> triplets = new Dictionary<string, int>();
+            Parallel.ForEach(strings, word => WorkOnWord(word, triplets, ct));
+            int value = 0;
+            StringBuilder result = new StringBuilder();
+            foreach (KeyValuePair<string, int> pair in triplets)
+            {
+                if (ct.IsCancellationRequested)
+                    return null;
+                if (pair.Value > value)
+                {
+                    result = new StringBuilder(pair.Key);
+                    value = pair.Value;
+                }
+                else if (pair.Value == value)
+                {
+                    result.Append(',');
+                    result.Append(pair.Key);
+                }
+            }
+            return string.Format("{0}\t{1}", result.ToString(), value);
+        }
+        private void WorkOnWord(string word, Dictionary<string, int> dict, CancellationToken ct)
+        {
+            
+            string triplet;
+            for (int i = 0; i < word.Length - 2; i++)
+            {
+                if (ct.IsCancellationRequested)
+                return;
+                triplet = word.Substring(i, 3);
+                lock (syncRoot)
+                {
+                    if (dict.ContainsKey(triplet))
+                        dict[triplet]++;
+                    else
+                        dict.Add(triplet, 1);
+                }
+
+            }
+        }
+    }
+}
---
+++ FrequentTripletSearch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace FrequentTripletSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*CancellationTokenSource cts = new CancellationTokenSource();
+            FrequentTripletFinder find = new FrequentTripletFinder();
+            string input1 = "hello,ello,hell,ell,llo,llo";
+            string input2 = "maow,aow,hellaow,aowt,morning,mornaowing";
+            Console.WriteLine(find.MostFrequentTriplet(input1, cts.Token));
+            Console.WriteLine(find.MostFrequentTriplet(input2, cts.Token));
+            Console.ReadKey();*/
+        }
+   
+    }
+    
+}
